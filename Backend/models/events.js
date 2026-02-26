1279415c8d8748513987afd73baf1e3f39b9f047
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-02-26 06:00:31 +0530

    Backend --> Event Model added

---
+++ Backend/models/events.js
@@ -0,0 +1,79 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema({
+
+    eventImage:{
+        type:String,
+    },
+
+    eventTitle: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 3,
+        maxlength: 100,
+    },
+
+    eventDescription: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 10,
+        maxlength: 1000,
+    },
+
+    eventStartDate: {
+        type: Date,
+        required: true,
+    },
+
+    eventEndDate: {
+        type: Date,
+        required: true,
+        validate: {
+            validator: function(value) {
+                return value > this.eventStartDate;
+            },
+            message: "End date must be after start date."
+        }
+    },
+
+    eventParticipantsList: [{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    }],
+
+    eventMaxParticipants: {
+        type: Number,
+        required: true,
+        min: 1,
+    },
+
+    eventLocation: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    isEventActive: {
+        type: Boolean,
+        default: true,
+    },
+
+    eventCreatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    eventMode:{
+        type:String,
+        enum:["Online","Offline"],
+        default:"Offline"
+    }
+
+}, {
+    timestamps: true,
+});
+
+module.exports = mongoose.model("Event", eventSchema);
