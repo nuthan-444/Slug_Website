354bc0736b373e6a87dc43c711002681efc6c1a2
NuthaN-444 <nuthanprasadkg@gmail.com>
2026-03-06 05:17:36 +0530

    backend ---> server.js added

---
+++ Backend/server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const cors = require("cors");
+const express = require("express");
+const connectDB = require('./config/db');
+const app = express();
+
+PORT = process.env.PORT || 5000; // assigning port
+
+
+app.use(express.json()); // alows json format
+app.use(cors()); // allows requests from different url
+connectDB();
+
+// routers
+const authRouter = require('./routes/authRoute');
+const eventRouter = require('./routes/eventRoute');
+
+
+// home page
+app.get("/" , (req,res) => {
+    res.send("hello");
+})
+
+
+
+// auth page route 
+app.use("/api/auth",authRouter);
+
+
+
+// event page route
+app.use("/api/events",eventRouter);
+
+
+
+
+app.listen(PORT , () => {
+    console.log(`Server is listening on Port ${PORT}`);
+})
